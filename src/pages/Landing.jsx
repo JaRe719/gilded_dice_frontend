0708520d274541ead46f3e3e702842b464bfec13
Janine Carl <janine.supercode@gmail.com>
2024-12-05 20:44:21 +0100

    add avatar choice panel functionality, no styling yet

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./Landing.css";
-import DiceRoller from '../components/DiceRoller/DiceRoller';
+// import DiceRoller from '../components/DiceRoller/DiceRoller';
+import AvatarChoice from '../components/Character/AvatarChoice';
 
 export default function Landing() {
   return (
     <div className='landing'>
       Landing Page
-    <DiceRoller />
+    <AvatarChoice />
     </div>
   )
 }
