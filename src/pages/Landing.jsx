34029b2d13befa72722d4dde382a3bf8d42738d9
Janine Carl <janine.supercode@gmail.com>
2024-12-05 23:24:37 +0100

    add skillTree functionality, styling tbd

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import "./Landing.css";
 // import DiceRoller from '../components/DiceRoller/DiceRoller';
 import AvatarChoice from '../components/Character/AvatarChoice';
+import SkillTree from '../components/Character/SkillTree';
 
 export default function Landing() {
   return (
     <div className='landing'>
       Landing Page
     <AvatarChoice />
+    <SkillTree />
     </div>
   )
 }
---
+++ src/utils/NpcProvider.jsx
@@ -0,0 +1,15 @@
+const npcPaths = [
+    "/assets/images/npc/banker.png",
+    "/assets/images/npc/builder.png",
+    "/assets/images/npc/dean.png",
+    "/assets/images/npc/realEstate.png",
+    "/assets/images/npc/mechanicBlue.png",
+    "/assets/images/npc/mechanicRed.png",
+    "/assets/images/npc/mother.png",
+    "/assets/images/npc/notary.png",
+    "/assets/images/npc/npcOne.png",
+    "/assets/images/npc/npcTwo.png",
+    "/assets/images/npc/roommate.png"
+  ];
+  
+  export const getNpcPaths = () => npcPaths;
